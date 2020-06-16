d7f2a4f69c72343fa671dcf37a8ff95273bc8f4f
Vadim Dmitriev <dva3213@gmail.com>
2020-06-16 20:27:01 +0300

    Добавил заглушки хэндлеров

--- router.go
+++ router.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
 )
 
 func newRouter() *fasthttprouter.Router {
 	router := fasthttprouter.New()
 
+	router.GET("/api/v1/streams", showStreams)
+	router.POST("/api/v1/streams", createStream)
+	router.DELETE("/api/v1/streams/:uid", deleteStream)
+	router.PATCH("/api/v1/streams/:uid", changeStreamState)
+
 	return router
+}
+
+func createStream(ctx *fasthttp.RequestCtx) {
+
+}
+
+func deleteStream(ctx *fasthttp.RequestCtx) {
+
+}
+
+func showStreams(ctx *fasthttp.RequestCtx) {
+
+}
+
+func changeStreamState(ctx *fasthttp.RequestCtx) {
+
 }
