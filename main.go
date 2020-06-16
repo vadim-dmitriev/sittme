ec53df0320842f22ad233a464b81e0925450944a
Vadim Dmitriev <dva3213@gmail.com>
2020-06-16 20:11:36 +0300

    Добавил запуск и завершение пустого сервера

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, os.Interrupt)
+	signal.Notify(quitCh, syscall.SIGTERM)
+
+	server := fasthttp.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Handler:      newRouter().Handler,
+	}
+
+	log.Printf("Service started")
+	go server.ListenAndServe(":8080")
+
+	// Reciving signal
+	<-quitCh
+	log.Printf("Gracefully terminating service...")
+	if err := server.Shutdown(); err != nil {
+		panic(err)
+	}
+	// TODO: wait for goroutines done -> use waitGroup
 }
---
+++ router.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/buaazp/fasthttprouter"
+)
+
+func newRouter() *fasthttprouter.Router {
+	router := fasthttprouter.New()
+
+	return router
+}
